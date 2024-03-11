083aa477529401a8b5fab478d4e54c43c21d47b0
Grinchs <marcisg2345@gmail.com>
2024-03-11 21:33:21 +0200

    Add project files.

---
+++ 4_rekursija_MGrīnfeldsITIA3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_rekursija_MGrīnfeldsITIA3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadiet skaitli:");
+            int number = int.Parse(Console.ReadLine());
+            int sum = Summa(number);
+            Console.WriteLine($"Ciparu summa ir: {sum}");
+        }
+
+        static int Summa(int number)
+        {
+            // Ja skaitlis ir vienaciparu, atgriežam to
+            if (number < 10)
+            {
+                return number;
+            }
+            // Atgriežam pēdējo ciparu plus atkārtoti izsauktu funkciju ar atlikušo skaitli
+            return number % 10 + Summa(number / 10);
+        }
+    }
+}
